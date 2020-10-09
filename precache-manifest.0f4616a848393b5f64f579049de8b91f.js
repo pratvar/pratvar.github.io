1b370682283e240bbf9395bc164d25e1a8f66a05
Pratik Var <npc.prat@gmail.com>
2020-10-09 22:06:02 +0530

    Updates

---
+++ precache-manifest.0f4616a848393b5f64f579049de8b91f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f14fbf302922c577ec50a63862f1b4e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "785691704dd546ff1594",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bd22fbe446fbfcef3e04",
+    "url": "/static/js/2.85f7a915.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.85f7a915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "785691704dd546ff1594",
+    "url": "/static/js/main.a25211dc.chunk.js"
+  },
+  {
+    "revision": "b7d71e30152f395c8adf",
+    "url": "/static/js/runtime-main.0004cdda.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
